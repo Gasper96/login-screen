2a7f61a6489aef90ba2b4de04ba1d95df7e40bcd
Gabueno <112331277+Gasper96@users.noreply.github.com>
2025-08-07 09:53:35 -0300

    Add files via upload

--- desafio1/welcome.php
+++ desafio1/welcome.php
@@ -1,17 +1,15 @@
-<?php
-echo 'BEM VINDO!';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>BEM VINDO</h1>
     <form action="logaut.php">
-        <button>SAIR</button>
+        <button type="submit">SAIR</button>
     </form>
 </body>
 </html>
